78f3de75576b306c09c9e2072cd93909d7293865
pkotsorgios <161494302+PanagiotisKotsorgios@users.noreply.github.com>
2026-03-18 20:29:26 +0200

    Delete gui/scripts directory

--- gui/scripts/fetch_imgui.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# gui/scripts/fetch_imgui.sh
-# Downloads the required Dear ImGui source files into gui/third_party/imgui/
-# Run once after cloning the repository.
-#
-# Requirements: git or curl
